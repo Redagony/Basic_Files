7893aec8b4ee0f00d21c49c5455d789c19c50d37
ChanMiLim <50476107+Redagony@users.noreply.github.com>
2021-10-11 21:22:37 +0900

    Biasic file for Java
    그냥 메모용 파일. 프로젝트 x

---
+++ Java_Basic.java
@@ -0,0 +1,103 @@
+// run java 누르기
+public class Java_Basic {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello Java World");    
+    }
+    
+    // 문법 구성 요소
+    // 식별자
+    // 키워드
+
+
+    // 자료형
+    //기본형
+    /// 참조형
+    // null
+    // 객체의 메모리 위치 주소값
+
+
+    //변수
+    // 속성변수
+    // 지역변수
+    // 매개변수
+    // 변수선언
+    //할당
+    // 초기화
+
+    //제어문
+    //if
+    //switch ~break,continue문
+    // for문
+    // while문
+    // do while문
+    // 향상된 for문
+
+    //클래스
+    // 멤버
+    // 속성변수
+    // 생성자
+    //메소드
+    // 객체 
+    // return
+    //  기본자료형
+    //  클래스명
+    //  인터페이스명
+    //  void
+
+    // 상속
+    // super예약어
+    // this 예약어
+    // 추상클래스
+    //  종단클래스
+    
+    // 인터페이스
+    //  인터페이스
+    //  overloading
+    // overriding
+    //  인터페이스의 상속
+    //  구현(implement)
+
+    // Array
+    // getter/setter 메소드
+    
+    // Collection
+    // generics
+    // DTO: data tranfer object
+    // DAO :data access object
+    // HashMap
+    // ArrayList
+
+
+    // Exception
+    // try구문
+    // catch구문
+    // finally구문
+    // Exception 
+    // ArithmeticException
+    // NumberFormatException
+
+    // String 객체
+
+
+    // 자바 클래스 형식
+    // 1. package 패키지명
+    // 2. import 수입클래스패키지명.수입클래스명
+    // 3. public
+    // 4. abstract/final
+    // 5. class 클래스명
+    // 6. public/protected/defalt/private 
+    // 7. static
+    // 8. final
+    // 9. 기본자료형/클래스명/인터페이스명 속성변수명
+    // 10. public/ protected / defalt / private 
+    // 11. 생성자명
+    // 12. ([자료형 매개변수,~])
+    // 13. pubic/protected/defalt/private
+    // 14. static
+    // 15. synchronized
+    // 16. abstract/final
+    // 17. 기본자료형/클래스명/인터페이스명void메소드명
+    // 18. 자료형 매개변수
+    
+}
